bd5072c007d382646b5914c57d934b7d74cab5ce
Filowannabe <ccfelipe97@gmail.com>
2023-08-20 20:18:47 -0500

    Implementing user auth middleware and apps

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import os
 import environ
@@ -45,12 +46,16 @@ DJANGO_APPS = [
 ]
 
 PROJECT_APPS = [
-
+    'apps.user'
 ]
     
 THIRD_PARTY_APPS = [
     'corsheaders',
     'rest_framework',
+    'djoser',
+    'social_django',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'ckeditor',
     'ckeditor_uploader'
 ]
@@ -137,7 +142,46 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+"django.contrib.auth.hashers.Argon2PasswordHasher",
+"django.contrib.auth.hashers.PBKDF2PasswordHasher",
+"django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+"django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
 
+DJOSER = {
+'LOGIN_FIELD': 'email',
+'USER_CREATE_PASSWORD_RETYPE': True,
+'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
+'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
+'SEND_CONFIRMATION_EMAIL': True,
+'SEND_ACTIVATION_EMAIL': True,
+'SET_USERNAME_RETYPE': True,
+'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+'SET_PASSWORD_RETYPE': True,
+'PASSWORD_RESET_CONFIRM_RETYPE': True,
+'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
+'ACTIVATION_URL': 'activate/{uid}/{token}',
+'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',
+'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['http://localhost:8000/google', 'http://localhost:8000/facebook'],
+'SERIALIZERS': {
+'user_create': 'apps.user.serializers.UserSerializer',
+'user': 'apps.user.serializers.UserSerializer',
+'current_user': 'apps.user.serializers.UserSerializer',
+'user_delete': 'djoser.serializers.UserDeleteSerializer',
+},
+}
+
+SIMPLE_JWT = {
+'AUTH_HEADER_TYPES': ('JWT', ),
+'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10080),
+'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+'ROTATE_REFRESFH_TOKENS':True,
+'BLACKLIST_AFTER_ROTATION': True,
+'AUTH_TOKEN_CLASSES': (
+'rest_framework_simplejwt.tokens.AccessToken',
+)
+}
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -180,6 +224,7 @@ EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+AUTH_USER_MODEL = "user.UserAccount"
 
 
 # Needed for 'debug' to be available inside templates.
